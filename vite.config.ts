d521bde323ffd585d3a90576e18cdad9171db3c8
Zyy <No Email>
2025-09-04 18:05:43 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
+
+import checker from "vite-plugin-checker";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+    checker({
+      eslint: {
+        lintCommand: "eslint .",
+        useFlatConfig: true,
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
